cb7ccb515b4dfc9e1974c2be793b0e35288e8c94
aliMohamed2345 <eloymohamedalimansor@gmail.com>
2025-05-23 19:41:31 +0300

    adding some changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
+app.get("/", (req, res) => {
+    try {
+        res.status(200).send({ success: true, message: `hello world` });
+    } catch (error) {
+        res.status(400).send({ success: true, message: `internal server error` });
+    }
+})
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;

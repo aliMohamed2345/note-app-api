50a41567048e3c96c8cec54e961fcc7b4281c265
aliMohamed2345 <eloymohamedalimansor@gmail.com>
2025-05-23 19:42:52 +0300

    some changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const env = require('dotenv')
 const app = express();
+
+env.config()
+
+app.use(express.json())
+
+const PORT = process.env.PORT | 3000
+
+
 app.get("/", (req, res) => {
     try {
         res.status(200).send({ success: true, message: `hello world` });
     } catch (error) {
         res.status(400).send({ success: true, message: `internal server error` });
     }
+
 })
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(PORT, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
